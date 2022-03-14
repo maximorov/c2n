99b55cd99aee8d16a27c4e6140ec7ff7397b756a
maks <maximorov@gmail.com>
2022-03-14 23:09:38 +0200

    revert circle to point

--- app/domains/user/executor/entity.go
+++ app/domains/user/executor/entity.go
@@ -1,9 +1,14 @@
 package executor
 
+import (
+	"helpers/app/core/db"
+)
+
 type UserExecutor struct {
-	UserId int64       `json:"user_id" table_name:"users_executors"`
-	Area   interface{} `json:"area"`
-	City   string      `json:"city"`
+	UserId   int64    `json:"user_id" table_name:"users_executors"`
+	Position db.Point `json:"position"`
+	Area     int      `json:"area"`
+	City     string   `json:"city"`
 }
 
 func (s *UserExecutor) IsEntity() {}
