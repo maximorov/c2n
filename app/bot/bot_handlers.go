fdb9056225c0b1b8050bd9c7da84a00509ad32e3
maks <maximorov@gmail.com>
2022-03-16 20:07:21 +0200

    reopen task action

--- app/bot/bot_handlers.go
+++ app/bot/bot_handlers.go
@@ -2,6 +2,7 @@ package bot
 
 import (
 	"context"
+	"fmt"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"go.uber.org/zap"
 	"helpers/app/core"
@@ -40,54 +41,53 @@ func (s *CallbackHandler) Handle(ctx context.Context, u tgbotapi.Update) bool {
 	usr := ctx.Value(`user`).(*user.User)
 	handled := false
 
-	switch {
-	case strings.Contains(u.CallbackData(), `hide:`) || strings.Contains(u.CallbackData(), `accept:`):
-		handled = true
-		parsed := strings.Split(u.CallbackData(), `:`)
-		action := parsed[0]
-		taskId, _ := strconv.Atoi(parsed[1])
+	callbackData := u.CallbackData()
+	if !strings.Contains(callbackData, `:`) {
+		return false
+	}
 
-		switch action {
-		case `accept`:
-			err := s.TaskActivityService.CreateActivity(ctx, usr.ID, taskId, `taken`)
-			if err != nil {
-				zap.S().Error(err)
-			}
-			msg := tgbotapi.NewMessage(u.CallbackQuery.Message.Chat.ID, `Завдання треба виконати за добу`)
-			s.Ans(msg)
-			s.informNeedy(ctx, taskId, `taken`)
-		case `hide`:
-			err := s.TaskActivityService.CreateActivity(ctx, usr.ID, taskId, `hidden`)
-			if err != nil {
-				zap.S().Error(err)
-			}
-			msg := tgbotapi.NewMessage(u.CallbackQuery.Message.Chat.ID, `Це завдання візьме хтось інший`)
-			s.Ans(msg)
-		}
-	case strings.Contains(u.CallbackData(), `complete:`) || strings.Contains(u.CallbackData(), `refuse:`):
-		handled = true
-		parsed := strings.Split(u.CallbackData(), `:`)
-		action := parsed[0]
-		taskId, _ := strconv.Atoi(parsed[1])
+	handled = true
+	parsed := strings.Split(u.CallbackData(), `:`)
+	action := parsed[0]
+	taskId, _ := strconv.Atoi(parsed[1])
 
-		switch action {
-		case `complete`:
-			err := s.TaskActivityService.UpdateActivity(ctx, usr.ID, taskId, `completed`)
-			if err != nil {
-				zap.S().Error(err)
-			}
-			msg := tgbotapi.NewMessage(u.CallbackQuery.Message.Chat.ID, `Дякую`)
-			s.Ans(msg)
-			s.informNeedy(ctx, taskId, `complete`)
-		case `refuse`:
-			err := s.TaskActivityService.UpdateActivity(ctx, usr.ID, taskId, `refused`)
-			if err != nil {
-				zap.S().Error(err)
-			}
-			msg := tgbotapi.NewMessage(u.CallbackQuery.Message.Chat.ID, `Візьме хтось інший, чи ні`)
-			s.Ans(msg)
-			s.informNeedy(ctx, taskId, `refuse`)
+	switch action {
+	case `accept`:
+		err := s.TaskActivityService.CreateActivity(ctx, usr.ID, taskId, `taken`)
+		if err != nil {
+			zap.S().Error(err)
 		}
+		msg := tgbotapi.NewMessage(u.CallbackQuery.Message.Chat.ID, `Завдання треба виконати за добу`)
+		s.Ans(msg)
+		s.informNeedy(ctx, taskId, `taken`)
+	case `hide`:
+		err := s.TaskActivityService.CreateActivity(ctx, usr.ID, taskId, `hidden`)
+		if err != nil {
+			zap.S().Error(err)
+		}
+		msg := tgbotapi.NewMessage(u.CallbackQuery.Message.Chat.ID, `Це завдання візьме хтось інший`)
+		s.Ans(msg)
+	case `complete`:
+		err := s.TaskActivityService.UpdateActivity(ctx, usr.ID, taskId, `completed`)
+		if err != nil {
+			zap.S().Error(err)
+		}
+		msg := tgbotapi.NewMessage(u.CallbackQuery.Message.Chat.ID, `Дякую`)
+		s.Ans(msg)
+		s.informNeedy(ctx, taskId, `complete`)
+	case `refuse`:
+		err := s.TaskActivityService.UpdateActivity(ctx, usr.ID, taskId, `refused`)
+		if err != nil {
+			zap.S().Error(err)
+		}
+		msg := tgbotapi.NewMessage(u.CallbackQuery.Message.Chat.ID, `Візьме хтось інший, чи ні`)
+		s.Ans(msg)
+		s.informNeedy(ctx, taskId, `refuse`)
+	case `reopen`:
+		s.informNeedy(ctx, taskId, `reopen`)
+	default:
+		zap.S().Error(`callback didnt processed`)
+		handled = false
 	}
 
 	return handled
@@ -129,6 +129,14 @@ func (s *CallbackHandler) informNeedy(ctx context.Context, tId int, status strin
 	case `taken`:
 		msg.Text = `Волонтер вляз вашу задачу в работу. Если в течении нескольких часов с вами не связались, нажмите ` + ReopenText
 		err := s.TaskUseCase.UpdateTaskStatus(ctx, tId, `in_progress`)
+		if err != nil {
+			zap.S().Error(err)
+			return
+		}
+	case `reopen`:
+		msg.Text = fmt.Sprintf("ваша задача %d переоткрыта", tId)
+		msg.ReplyMarkup = ToMainKeyboard
+		err := s.TaskUseCase.UpdateTaskStatus(ctx, tId, `new`)
 		if err != nil {
 			zap.S().Error(err)
 			return
